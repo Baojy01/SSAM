f7eb2a6478cc00fa67f2478e433669eb39b99597
Bob <51505999+Baojy01@users.noreply.github.com>
2025-05-11 15:58:08 +0800

    Update train.py

--- Det/train.py
+++ Det/train.py
@@ -1,33 +1,35 @@
 import warnings
 import os
 from ultralytics import YOLO
-
 warnings.filterwarnings('ignore')
 
 if __name__ == '__main__':
-    archs = ['yolo11_mobilenetv2.yaml', 'yolo11_efficientformerv2.yaml', 'yolo11_mobilevit.yaml', 'yolo11_edgevit.yaml']
-    dir_paths = ['mb_20', 'ef_s2', 'mbvit_s', 'edgevit_s']
+    
+    data_dir = "./mydata/rdd.yaml"
+    
+    # arch = 'yolo11_mobilenetv2.yaml' 
+    # arch = 'yolo11_efficientformerv2.yaml'
+    # arch = 'yolo11_mobilevit.yaml'
+    arch = 'yolo11_ssam.yaml'
+    
+    dir_path = 'runs/RDD/ssam_l'
 
-    for i in range(4):
-        arch = archs[i]
-        dir_path = os.path.join('runs/neu', dir_paths[i])
-
-    model = YOLO('yolo11_efficientformerv2.yaml', task='detect')  # yolo11_mobilenetv2.yaml
-    model.train(data="./mydata/neu_det.yaml",
-                imgsz=256,
-                epochs=2,
+    model = YOLO(arch, task='detect')
+    model.train(data=data_dir,
+                imgsz=512,
+                epochs=100,
                 single_cls=False,
-                batch=32,
+                batch=16,
                 close_mosaic=0,
-                workers=0,
+                workers=4,
                 cache=False,
                 device='0',
                 optimizer='AdamW',
                 amp=False,
-                project='runs/train',
+                project=dir_path,
                 name='exp',
                 seed=42,
-                lr0=1e-3,
+                lr0=5e-3,
                 cos_lr=True,
                 weight_decay=1e-4,
                 )
