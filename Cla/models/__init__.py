573aeb18ec6555c6609fd571e46281b8c5130104
Bob <51505999+Baojy01@users.noreply.github.com>
2025-05-11 16:11:46 +0800

    Update __init__.py

--- Cla/models/__init__.py
+++ Cla/models/__init__.py
@@ -1,5 +1,5 @@
 from .ssam import SSAMNet_Tiny, SSAMNet_Small, SSAMNet_Base
-from .MobileNetV2 import mobilenet_v2_075, mobilenet_v2_10, mobilenet_v2_14, mobilenet_v2_20
+from .MobileNetV2 import mobilenet_v2_075, mobilenet_v2_10, mobilenet_v2_14, mobilenet_v2_20, mobilenet_v2_25
 from .fasternet import fasternet_t0, fasternet_t1, fasternet_t2
 from .edgevit import edgevit_xxs, edgevit_xs, edgevit_s
 from .edgenext import edgenext_xx_small, edgenext_x_small, edgenext_small
